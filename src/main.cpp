e11bdaaa96aaa89494e55119e2424deea6ea14bc
JustusKirsche <jkirsche@web.de>
2025-02-05 18:10:23 +0100

    Mehr Lampen mehr testen.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,27 @@
 #include <Arduino.h>
 
+#define LED_GREEN 23
+#define LED_RED 22
+#define LED_BLUE 21
+void led_blink(int pin);
 void setup() {
   // put your setup code here, to run once:
-  pinMode (22, OUTPUT);
+  pinMode (LED_GREEN, OUTPUT);
+  pinMode (LED_RED, OUTPUT);
+  pinMode (LED_BLUE, OUTPUT);
+  void led_blink(int pin);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(22, 1);
-  delay(1000);
-  digitalWrite(22, 0);
-  delay(1000);
+  led_blink(LED_GREEN);
+  led_blink(LED_RED);
+  led_blink(LED_BLUE); 
+}
+
+void led_blink(int pin) {
+  digitalWrite(pin, HIGH);
+  delay(500);
+  digitalWrite(pin, LOW);
+  delay(500);
 }
