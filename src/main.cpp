ea7fa7cfd638ff70ceb0145f86737cb560b3a2c2
JustusKirsche <jkirsche@web.de>
2025-02-07 12:30:13 +0100

    Serial Monitor hinzugefügt.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 //here we define the functions
 void led_blink(int pin);
 void setup() {
+  Serial.begin(115200);
   // put your setup code here, to run once:
   pinMode (LED_GREEN, OUTPUT);
   pinMode (LED_RED, OUTPUT);
@@ -17,12 +18,27 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Blinking green LED");
   led_blink(LED_GREEN);
+  Serial.println("Blinking red LED");
   led_blink(LED_RED);
+  Serial.println("Blinking blue LED");
   led_blink(LED_BLUE); 
 }
 
+/**
+ * @brief Blinks an LED connected to the specified pin.
+ *
+ * This function sets the specified pin to HIGH for 500 milliseconds, 
+ * then sets it to LOW for 500 milliseconds, creating a blinking effect. 
+ * It assumes the pin has been configured as an OUTPUT.
+ *
+ * @param pin The pin number connected to the LED.
+ */
+
 void led_blink(int pin) {
+  Serial.print("Blinking LED connected to pin ");
+  Serial.println(pin);
   digitalWrite(pin, HIGH);
   delay(500);
   digitalWrite(pin, LOW);
