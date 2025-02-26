90655bb9c1afee21131f0ba4c29b7d7450e95f54
JustusKirsche <jkirsche@web.de>
2025-02-26 11:05:28 +0100

    Zweiscehnstand (Problem C++ kann nicht einfach bei onclick aufgerufen werden wir brauchen einen workaround) Codium( In this example, the button's onclick attribute is set to navigate to the /setdirection URL with a direction parameter set to RIGHT. The handleSetDirection method is then called to handle this request, which calls the setdirection method with the corresponding direction. )

--- src/main.cpp
+++ src/main.cpp
@@ -30,43 +30,35 @@ class CaptiveRequestHandler : public AsyncWebHandler {
       return true;
     }
   
-/*************  ✨ Codeium Command ⭐  *************/
-    /**
-     * Handles incoming web server requests and responds with a captive portal page.
-     * 
-     * This method creates an HTML response stream that presents a simple web page,
-     * informing the user that they have reached the captive portal. It includes the
-     * original URL the user attempted to reach and provides a link to the network's
-     * access point.
-     * 
-     * @param request The incoming web request to be handled.
-     */
-
-/******  6150e7a6-bbf1-4a91-b17e-0346f578afdd  *******/
     void handleRequest(AsyncWebServerRequest *request) {
       AsyncResponseStream *response = request->beginResponseStream("text/html");
       response->print("<!DOCTYPE html><html><head><title>Captive Portal</title></head><body>");
       response->print("<p>This is out captive portal front page.</p>");
       response->printf("<p>You were trying to reach: http://%s%s</p>", request->host().c_str(), request->url().c_str());
       response->printf("<p>Try opening <a href='http://%s'>this link</a> instead</p>", WiFi.softAPIP().toString().c_str());
-      response->print("</body></html>");
+      response->printf("<html><body><button onclick=\"location.href='/setdirection?direction=RIGHT'\">Rechts</button></body></html>");
       request->send(response);
     }
+    
+    // Add a new handler for the /setdirection request
+    void handleSetDirection(AsyncWebServerRequest *request) {
+      String direction = request->getParam("direction")->value();
+      if (direction == "RIGHT") {
+        setdirection(RIGHT);
+      } else if (direction == "LEFT") {
+        setdirection(LEFT);
+      } else if (direction == "STRAIGHT") {
+        setdirection(STRAIGHT);
+      }
+      request->send(200, "text/plain", "Direction set");
+    }
+    
+    // Add the new handler to the server
+    server.addHandler(new AsyncCallbackWebHandler("/setdirection", [](AsyncWebServerRequest *request) {
+      handleSetDirection(request);
+    }));
   };
 
-//12V Motor
-#define PINPWM1 27
-#define PINPWM2 26
-
-#define channel1 0  // 0 bis 15 PWM-Kanäle stehen zur Auswahl
-#define channel2 1  // 0 bis 15 PWM-Kanäle stehen zur Auswahl
-#define frequency 1000 
-#define resolution 8
-#define dutycycle (pow(2,resolution)-1) 
-
-enum Gear {REVERS, DRIVE, NUTRAL};
-Gear gear = NUTRAL;
-//ServoMotor
 #define SMOTOR 32
 Servo servo;
 enum Directions {RIGHT, LEFT, STRAIGHT};
@@ -83,30 +75,16 @@ void setup() {
   //more handlers...
   server.begin();
 
-  // Set the PWM pin for channel 1 as an output
-  pinMode(PINPWM1, OUTPUT);
-  // Configure PWM settings for channel 1
-  ledcSetup(channel1, frequency, resolution);
-  // Attach PWM channel 1 to the specified pin
-  ledcAttachPin(PINPWM1, channel1);
 
-  // Set the PWM pin for channel 2 as an output
-  pinMode(PINPWM2, OUTPUT);
-  // Configure PWM settings for channel 2
-  ledcSetup(channel2, frequency, resolution);
-  // Attach PWM channel 2 to the specified pin
-  ledcAttachPin(PINPWM2, channel2);
+  //Servomotor
+  // Set the servo motor pin mode to output
+  pinMode(SMOTOR, OUTPUT);
 
+  // Attach the servo motor to the specified pin
+  servo.attach(SMOTOR);
 
-//Servomotor
-// Set the servo motor pin mode to output
-pinMode(SMOTOR, OUTPUT);
-
-// Attach the servo motor to the specified pin
-servo.attach(SMOTOR);
-
-// Set the initial position of the servo motor to 90 degrees
-servo.write(90);
+  // Set the initial position of the servo motor to 90 degrees
+  servo.write(90);
 }
 
 /**
@@ -117,6 +95,9 @@ servo.write(90);
  * - If the gear is set to REVERS, it writes the duty cycle to channel2 for reverse motion.
  * - If the gear is set to NUTRAL, no action is taken.
  */
+void setdirection(Directions directionf) {
+  direction = directionf;
+}
 
 void loop() {
   // put your main code here, to run repeatedly: 
@@ -124,16 +105,17 @@ void loop() {
   dnsServer.processNextRequest();
 
   //Fahren mit Servo
-  switch(gear) {
-    case DRIVE:
-      ledcWrite(channel1,dutycycle);
-    break;
-    case REVERS:
-      ledcWrite(channel2,dutycycle);
-    break;
-    case NUTRAL:
-    break;
-   }
+  switch(direction) {
+    case RIGHT:
+      servo.write(60);
+      break;
+    case LEFT:
+      servo.write(120);
+      break;
+    case STRAIGHT:
+      servo.write(90);
+      break;
+  }
 
    
 }
